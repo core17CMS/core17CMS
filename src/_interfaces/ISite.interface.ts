5b62eb906321197783012c82243aac94a582cd0d
ramcd86 <ra_mcd@yahoo.com>
2020-03-06 11:48:23 +0000

    Completion of global interface.

--- src/_interfaces/ISite.interface.ts
+++ src/_interfaces/ISite.interface.ts
@@ -38,48 +38,42 @@ export interface IRouteResponse {
   id: string;
 }
 
+export interface IDatabaseQueryResolution {
+  status: string;
+  payload: any;
+}
 
-      // AREA INTERFACES //
 
-export interface IBlogArea {
+
+// AREA INTERFACES //
+
+interface IAreaBase {
   areaName: string;
   type: string;
   options: any;
-  title: string;
-  subTitle: string;
-  text: string;
+}
+
+export interface IBlogArea extends IAreaBase {
   allowedTypes: string[];
   blogArea: Array<IFeatureElement | IBlogElement>[];
 }
 
-export interface IButtonArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IButtonArea extends IAreaBase {
   allowedTypes: string[];
-  guttonArea: Array<IButtonElement>[];
+  buttonArea: Array<IButtonElement>[];
 }
 
-export interface IFeatureArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IFeatureArea extends IAreaBase {
   allowedTypes: string[];
   featureArea: Array<IFeatureElement>[];
 }
 
-export interface IFooterArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IFooterArea extends IAreaBase {
   allowedTypes: string[];
   footerArea: Array<IImageElement | ITextElement | ILinklistElement>[];
 }
 
-export interface IGenericArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IGenericArea extends IAreaBase {
   title: string;
   subTitle: string;
   text: string;
@@ -87,28 +81,19 @@ export interface IGenericArea {
   genericArea: Array<any>[];
 }
 
-export interface IJumbotronArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IJumbotronArea extends IAreaBase {
   allowedTypes: string[];
   jumbotronArea: Array<IJumbotronElement | IImageElement | ITextElement | IButtonElement>[];
 }
 
-export interface ILinklistArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface ILinklistArea extends IAreaBase {
   title: string;
   subTitle: string;
   allowedTypes: string[];
   linklistArea: Array<ILinklistElement | IButtonElement>[];
 }
 
-export interface INewsArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface INewsArea extends IAreaBase {
   title: string;
   subTitle: string;
   text: string;
@@ -116,10 +101,7 @@ export interface INewsArea {
   newsArea: Array<IFeatureElement | INewsElement>[];
 }
 
-export interface IFormArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IFormArea extends IAreaBase {
   title: string;
   subTitle: string;
   text: string;
@@ -127,57 +109,125 @@ export interface IFormArea {
   formArea: Array<ITextElement | IButtonElement | IFormElement>[];
 }
 
-export interface IHeaderArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface IHeaderArea extends IAreaBase {
   allowedTypes: string[];
   headerArea: Array<ITextElement | IImageElement | ILinklistElement>[];
 }
 
-export interface ITextArea {
-  areaName: string;
-  type: string;
-  options: any;
+export interface ITextArea extends IAreaBase {
   allowedTypes: string[];
   textArea: ITextElement[];
 }
 
-      // ELEMENT INTERFACES //
 
-export interface IBlogElement {}
+// ELEMENT INTERFACES //
 
-export interface IJumbotronElement {}
-
-export interface IFeatureElement {}
-
-export interface INewsElement {}
-
-export interface ITextElement {}
-
-export interface IImageElement {}
-
-export interface ILinklistElement {}
-
-export interface IButtonElement {}
-
-export interface IFormElement {}
-
-export interface IGenericElement {
+interface IElementBase {
   elementName: string;
-  genericElement: string;
+  type: string;
+  options: any;
 }
 
-export interface IGenericContent {
-  areaName: string;
-  genericArea: IGenericElement[];
+export interface IGenericElement extends IElementBase {
+  genericElement: IGenericElementFunctionality;
+}
+
+export interface IGenericElementFunctionality {
+  content: string;
+}
+
+export interface ILinklistElement extends IElementBase {
+  linklistElement: ILinklistElementFunctionality;
+}
+
+export interface ILinklistElementFunctionality {
+  linklistTitle: string;
+  linkList: ILinklistElementLinkListActual[];
+}
+
+export interface ILinklistElementLinkListActual {
+  linkText: string;
+  linkUrl: string;
+}
+
+
+export interface IButtonElement extends IElementBase {
+  buttonElement: IButtonElementFunctionality;
+}
+
+export interface IButtonElementFunctionality {
+  buttonText: string;
+  buttonUrl: string;
+}
+
+export interface IJumbotronElement extends IElementBase {
+  jumbotronElement: IJumbotronElementFunctionality;
+}
+
+export interface IJumbotronElementFunctionality {
+  image: any;
+  video: any;
+  title: string;
+  subtitle: string;
+  buttonText: string;
+  buttonUrl: string;
+}
+
+export interface IFeatureElement extends IElementBase {
+  featureElement: IFeatureElementFunctionality;
+}
+
+export interface IFeatureElementFunctionality {
+  image: any;
+  title: string;
+  text: string;
+  buttonText: string;
+  buttonUrl: string;
+}
+
+export interface IImageElement extends IElementBase {
+  imageElement: IImageElementFunctionality;
+}
+export interface IImageElementFunctionality {
+  image: any;
+}
+
+export interface ITextElement extends IElementBase {
+  textElement: ITextElementFunctionality;
+}
+export interface ITextElementFunctionality {
+  text: string;
+}
+
+export interface IBlogElement extends IElementBase {
+  blogElement: IBlogElementFunctionality;
+}
+export interface IBlogElementFunctionality {
+  title: string;
+  text: string;
+  date: string;
+}
+
+export interface INewsElement extends IElementBase {
+  newsElement: INewsElementFunctionality;
+}
+export interface INewsElementFunctionality {
+  title: string;
+  text: string;
+  date: string;
+}
+
+export interface IFormElement extends IElementBase {
+  formElement: IFormElementFunctionality;
+}
+export interface IFormElementFunctionality {
+  title: string;
+  text: string;
+  form: any[];
+  submitButtonText: string;
 }
 
 
 
 
 
-export interface IDatabaseQueryResolution {
-  status: string;
-  payload: any;
-}
