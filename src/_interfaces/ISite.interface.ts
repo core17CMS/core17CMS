7ad7b1e079cb9ffa2bf517f951208d760a852158
ramcd86 <ra_mcd@yahoo.com>
2020-03-06 11:07:45 +0000

    Ongoing work on interfaces.

--- src/_interfaces/ISite.interface.ts
+++ src/_interfaces/ISite.interface.ts
@@ -30,16 +30,6 @@ export interface ISiteContentItems {
   areas: IGenericContent[]
 }
 
-export interface IGenericContent {
-  areaName: string;
-  genericArea: IGenericElement[];
-}
-
-export interface IGenericElement {
-  elementName: string;
-  genericElement: string;
-}
-
 export interface ISiteOptions {
   template: string;
 }
@@ -47,6 +37,143 @@ export interface ISiteOptions {
 export interface IRouteResponse {
   id: string;
 }
+
+
+      // AREA INTERFACES //
+
+export interface IBlogArea {
+  areaName: string;
+  type: string;
+  options: any;
+  title: string;
+  subTitle: string;
+  text: string;
+  allowedTypes: string[];
+  blogArea: Array<IFeatureElement | IBlogElement>[];
+}
+
+export interface IButtonArea {
+  areaName: string;
+  type: string;
+  options: any;
+  allowedTypes: string[];
+  guttonArea: Array<IButtonElement>[];
+}
+
+export interface IFeatureArea {
+  areaName: string;
+  type: string;
+  options: any;
+  allowedTypes: string[];
+  featureArea: Array<IFeatureElement>[];
+}
+
+export interface IFooterArea {
+  areaName: string;
+  type: string;
+  options: any;
+  allowedTypes: string[];
+  footerArea: Array<IImageElement | ITextElement | ILinklistElement>[];
+}
+
+export interface IGenericArea {
+  areaName: string;
+  type: string;
+  options: any;
+  title: string;
+  subTitle: string;
+  text: string;
+  allowedTypes: string[];
+  genericArea: Array<any>[];
+}
+
+export interface IJumbotronArea {
+  areaName: string;
+  type: string;
+  options: any;
+  allowedTypes: string[];
+  jumbotronArea: Array<IJumbotronElement | IImageElement | ITextElement | IButtonElement>[];
+}
+
+export interface ILinklistArea {
+  areaName: string;
+  type: string;
+  options: any;
+  title: string;
+  subTitle: string;
+  allowedTypes: string[];
+  linklistArea: Array<ILinklistElement | IButtonElement>[];
+}
+
+export interface INewsArea {
+  areaName: string;
+  type: string;
+  options: any;
+  title: string;
+  subTitle: string;
+  text: string;
+  allowedTypes: string[];
+  newsArea: Array<IFeatureElement | INewsElement>[];
+}
+
+export interface IFormArea {
+  areaName: string;
+  type: string;
+  options: any;
+  title: string;
+  subTitle: string;
+  text: string;
+  allowedTypes: string[];
+  formArea: Array<ITextElement | IButtonElement | IFormElement>[];
+}
+
+export interface IHeaderArea {
+  areaName: string;
+  type: string;
+  options: any;
+  allowedTypes: string[];
+  headerArea: Array<ITextElement | IImageElement | ILinklistElement>[];
+}
+
+export interface ITextArea {
+  areaName: string;
+  type: string;
+  options: any;
+  allowedTypes: string[];
+  textArea: ITextElement[];
+}
+
+      // ELEMENT INTERFACES //
+
+export interface IBlogElement {}
+
+export interface IJumbotronElement {}
+
+export interface IFeatureElement {}
+
+export interface INewsElement {}
+
+export interface ITextElement {}
+
+export interface IImageElement {}
+
+export interface ILinklistElement {}
+
+export interface IButtonElement {}
+
+export interface IFormElement {}
+
+export interface IGenericElement {
+  elementName: string;
+  genericElement: string;
+}
+
+export interface IGenericContent {
+  areaName: string;
+  genericArea: IGenericElement[];
+}
+
+
 
 
 
