45308c0975dabe4a634df79a459aa158b9f16824
ramcd86 <ra_mcd@yahoo.com>
2020-03-06 12:38:05 +0000

    More factories.

--- src/_factories/_area-factories/blogarea.factory.ts
+++ src/_factories/_area-factories/blogarea.factory.ts
@@ -1,14 +1,4 @@
-// BlogAreaFactory
-// ButtonAreaFactory
-// FeatureAreaFactory
-// FooterAreaFactory
-// FormAreaFactory
-// GenericAreaFactory
-// HeaderAreaFactory
-// JumbotronAreaFactory
-// NewsAreaFactory
-// TeaserAreaFactory
-// TextAreaFactory
+
 
 export class BlogAreaFactory {
 
